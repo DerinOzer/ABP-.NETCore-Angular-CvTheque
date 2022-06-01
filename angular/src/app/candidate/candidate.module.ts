ad0e5fc2a39abf7bf1b4163c5e943a6a2b4f9a5d
Derin OZER <derin.ozer@simphonis.com>
2022-06-01 11:30:54 +0200

    Useless references deleted.

--- angular/src/app/candidate/candidate.module.ts
+++ angular/src/app/candidate/candidate.module.ts
@@ -6,10 +6,6 @@ import { NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFileUploaderModule } from 'angular-file-uploader';
 import { HttpClientModule } from '@angular/common/http';
 import {MatTooltipModule} from '@angular/material/tooltip';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -20,11 +16,7 @@ import {MatButtonModule} from '@angular/material/button';
     NgbDatepickerModule,
     AngularFileUploaderModule,
     HttpClientModule,
-    MatTooltipModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatIconModule,
-    MatButtonModule
+    MatTooltipModule
   ]
 })
 export class CandidateModule { }
